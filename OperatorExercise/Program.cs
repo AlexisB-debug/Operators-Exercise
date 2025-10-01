9202bc2b6ac7315d80d719baa7ad17c4599b8f6f
Alexis Boegner <alexisboegner@gmail.com>
2025-09-30 21:10:10 -0400

    Thought Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,13 +6,13 @@
         {
             int a = 17;
             int b = 4;
-            
-            int sum = a+b;
-            int subtraction = a-b;
-            int multiplication = a*b;
-            
-            int quotient = a/b;
-            int remainder = a%b;
+
+            int sum = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
 
             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
 
@@ -23,8 +23,30 @@
 
         static double AreaOfCircle(double radius)
         {
-          double theAreaOftheCircle = Math.PI * Math.Pow(radius, 2);
-          return theAreaOftheCircle;
+            double theAreaOftheCircle = Math.PI * Math.Pow(radius, 2);
+            return theAreaOftheCircle;
+        }
+
+        static void ThoughtExercise()
+        {
+            Console.WriteLine("int i = 3\n int j = 4\n int k = ++i * j++\n What's the value of int k?");
+            int greenCheckMark = int.Parse(Console.ReadLine());
+            if (greenCheckMark = RedCrossMark())
+            {
+                Console.ReadLine("Green Check Mark!");
+            }
+            else if (greenCheckMark != RedCrossMark)
+            {
+                Console.WriteLine("Oops, Red Cross Mark! The correct answer is {greenCheckMark}!");
+            }
+        }
+
+        static int RedCrossMark()
+        {
+            int i = 3;
+            int j = 4;
+            int k = ++i * j++;
+            return k;
         }
     }
 }
