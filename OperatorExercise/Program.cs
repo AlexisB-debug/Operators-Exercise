4077c61a7d1864bbacab562ba7b35f65600398ab
Alexis Boegner <alexisboegner@gmail.com>
2025-09-30 20:30:05 -0400

    Arithmetic operations & calculating the area of a circle

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a+b;
+            int subtraction = a-b;
+            int multiplication = a*b;
+            
+            int quotient = a/b;
+            int remainder = a%b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What's the radius of the circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of the circle whose radius is {radius} is {AreaOfCircle(radius)}");
+        }
+
+        static double AreaOfCircle(double radius)
+        {
+          double theAreaOftheCircle = Math.PI * Math.Pow(radius, 2);
+          return theAreaOftheCircle;
         }
     }
 }
